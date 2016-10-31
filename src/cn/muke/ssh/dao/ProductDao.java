5dd118a1b5d126dc5d2647ef64c1b92cc22b5d51
gitxiao <xia198507@126.com>
2016-10-31 08:21:26 +0800

    添加ssh框架源码包

--- src/cn/muke/ssh/dao/ProductDao.java
+++ src/cn/muke/ssh/dao/ProductDao.java
@@ -1,5 +1,7 @@
 package cn.muke.ssh.dao;
 
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
 import cn.muke.ssh.domain.Product;
 
 /**
@@ -7,12 +9,13 @@ import cn.muke.ssh.domain.Product;
  * @author Cfrjkj
  *
  */
-public class ProductDao {
+public class ProductDao extends HibernateDaoSupport{
 
 	public void save(Product product){
 		System.out.println("ProductDao dao中的保存方法");
 		System.out.println("product.getName() = " + product.getPname());
 		System.out.println("product.getPid() = " + product.getPid());
 		System.out.println("product.getPrice() = " + product.getPrice());
+		this.getHibernateTemplate().save(product);
 	}
 }
